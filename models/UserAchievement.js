871babdb329899d35fd2f8644a87ae9abfdcbec1
RRocky23 <rrrocky2323@gmail.com>
2025-11-03 15:10:19 +0100

    modele2

---
+++ models/UserAchievement.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const userAchievementSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        type: Number,
+        required: true
+    },
+    achievementType: {
+        type: String,
+        required: true
+    },
+    achievementName: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    progressCurrent: {
+        type: Number,
+        required: true
+    },
+    progressTarget: {
+        type: Number,
+        required: true
+    },
+    unlocked: {
+        type: Boolean,
+        required: true
+    },
+    unlockedAt: {
+        type: Date,
+        required: true
+    }
+});
+
+const UserAchievement = mongoose.model("UserAchievement", userAchievementSchema);
+export default UserAchievement;
+
