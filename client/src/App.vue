e662406d846164d020fd186635ec0fdbf73699b4
Ihavenoideaforname <48954462+Ihavenoideaforname@users.noreply.github.com>
2025-11-07 18:10:19 +0100

    Home Page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="d-flex flex-column min-vh-100">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="app-container d-flex flex-column min-vh-100">
+    <nav class="navbar navbar-expand-lg navbar-opaque">
       <div class="collapse navbar-collapse">
-        <a class="navbar-brand" href="#">Pocket Monsters</a>
+        <a class="navbar-brand" href="/home">Pocket Monsters</a>
         <div>
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/profile">Profile</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/account">Account</router-link>
-            </li>
           </ul>
         </div>
       </div>
+      <div class="ms-auto">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/account/register">Sign Up</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/account/login">Sign In</router-link>
+            </li>
+        </ul>
+      </div>
     </nav>
 
-    <main class="flex-grow-1 my-5">
-      <router-view></router-view>
+    <main class="flex-grow-1 my-5 d-flex justify-content-center align-items-center" style="min-height: 70vh;">
+        <router-view></router-view>
     </main>
 
-    <footer class="bg-light text-dark py-4 mt-auto w-100">
+    <footer class="text-dark py-2 mt-auto w-100 footer-opaque">
       <div class="text-center">
         <p class="mb-0">© 2025 Pocket Monsters Project.</p>
         <small>Built with Node.js, Express, Vue, Vite</small>
@@ -34,3 +35,20 @@
 
 <script setup>
 </script>
+
+<style scoped>
+  .app-container {
+    background-image: url('/tmpPokemonBg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .navbar-opaque {
+    background-color: rgba(255, 255, 255, 0.85);
+  }
+
+  .footer-opaque {
+    background-color: rgba(255, 255, 255, 0.85);
+  }
+</style>
